627af3deef8409f7f4c0e3314632424da880e504
baehynebin <baehynebin@hotmail.co.kr>
2018-01-23 22:24:17 +0900

    Initial commit

---
+++ app/src/main/java/kr/purplebeen/noteapp/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package kr.purplebeen.noteapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kr.purplebeen.noteapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/kr/purplebeen/noteapp/activities/SplashActivity.kt
@@ -0,0 +1,19 @@
+package kr.purplebeen.noteapp.activities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kr.purplebeen.noteapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        var handler : Handler  = Handler()
+        handler.postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }, 3000)
+    }
+}
