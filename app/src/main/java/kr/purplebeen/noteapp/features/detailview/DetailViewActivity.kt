9217967e808429936efe72c139e55cf45603e73c
배현빈 <baehynebin@hotmail.co.kr>
2020-02-26 20:28:37 +0900

    디테일 뷰 MVVM 기반으로 리팩토링

--- app/src/main/java/kr/purplebeen/noteapp/features/detailview/DetailViewActivity.kt
+++ app/src/main/java/kr/purplebeen/noteapp/features/detailview/DetailViewActivity.kt
@@ -1,74 +1,46 @@
 package kr.purplebeen.noteapp.features.detailview
 
-import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_detail_view.*
-import kr.purplebeen.noteapp.Note
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import kr.purplebeen.noteapp.R
+import kr.purplebeen.noteapp.databinding.ActivityDetailViewBinding
 import kr.purplebeen.noteapp.features.edit.EditActivity
-import ninja.sakib.pultusorm.core.PultusORM
-import ninja.sakib.pultusorm.core.PultusORMCondition
 
 class DetailViewActivity : AppCompatActivity() {
     val position : Int by lazy {
         intent.extras.getInt("position")
     }
 
-    lateinit var note : Note
-    lateinit var pultusORM : PultusORM
+    lateinit var mBinding: ActivityDetailViewBinding
+    lateinit var mViewModel: DetailViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_view)
-
-        setORM()
-        loadData()
-        setListeners()
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail_view)
+        mViewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
+        mViewModel.loadData(position)
+        observeViewModel()
     }
 
-
-
-    fun setORM() {
-        val appPath : String = applicationContext.filesDir.absolutePath
-        pultusORM = PultusORM("note.db", appPath)
-    }
-
-    fun loadData() {
-        note = pultusORM.find(Note())[position] as Note
-        titleText.text = note.title
-        contentText.text  = note.content
-    }
-
-    fun setListeners() {
-        editButton.setOnClickListener{
+    fun observeViewModel() {
+        mViewModel.navigateToEditCallback.observe(this, Observer {
             var newIntent  : Intent = Intent(this@DetailViewActivity, EditActivity::class.java)
             newIntent.putExtra("position", position)
             startActivity(newIntent)
-        }
-        deleteButton.setOnClickListener {
-            val alert : AlertDialog = AlertDialog.Builder(this@DetailViewActivity).create()
-            alert.setTitle("안내")
-            alert.setMessage("정말로 삭제하시겠습니까?")
-            alert.setButton(AlertDialog.BUTTON_POSITIVE, "확인") { _, i ->
-                val condition: PultusORMCondition = PultusORMCondition.Builder()
-                        .eq("noteId", note.noteId)
-                        .build()
-                pultusORM.delete(Note(), condition)
-                Toast.makeText(applicationContext, "성공적으로 삭제되었습니다!", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-            alert.setButton(AlertDialog.BUTTON_NEGATIVE, "취소") { _, i ->
-                alert.dismiss()
-            }
-            alert.show()
-        }
-   }
+        })
+
+        mViewModel.finishCallback.observe(this, Observer {
+            finish()
+        })
+    }
 
     override fun onResume() {
         super.onResume()
-        loadData()
+        mViewModel.loadData(position)
     }
 
 }
---
+++ app/src/main/java/kr/purplebeen/noteapp/features/detailview/DetailViewModel.kt
@@ -0,0 +1,58 @@
+package kr.purplebeen.noteapp.features.detailview
+
+import android.app.AlertDialog
+import android.app.Application
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.ObservableField
+import androidx.lifecycle.AndroidViewModel
+import kr.purplebeen.noteapp.Note
+import kr.purplebeen.noteapp.R
+import kr.purplebeen.noteapp.mvvm.SingleLiveEvent
+import ninja.sakib.pultusorm.core.PultusORM
+import ninja.sakib.pultusorm.core.PultusORMCondition
+
+class DetailViewModel(application:Application): AndroidViewModel(application) {
+    lateinit var note : Note
+    var pultusORM : PultusORM
+    var appPath: String = ""
+
+    var titleField: ObservableField<String> = ObservableField()
+    var contentField: ObservableField<String> = ObservableField()
+    var navigateToEditCallback: SingleLiveEvent<Void> = SingleLiveEvent()
+    var finishCallback: SingleLiveEvent<Void> = SingleLiveEvent()
+
+    init {
+        appPath = getApplication<Application>().filesDir.absolutePath
+        pultusORM = PultusORM("note.db", appPath)
+    }
+
+    fun loadData(position: Int) {
+        note = pultusORM.find(Note())[position] as Note
+        titleField.set(note.title)
+        contentField.set(note.content)
+    }
+
+    fun onEditButtonClicked(view: View) {
+        navigateToEditCallback.call()
+    }
+
+    fun onDeleteButtonClicked(view: View) {
+        val alert : AlertDialog = AlertDialog.Builder(getApplication()).create()
+        alert.setTitle(getApplication<Application>().resources.getString(R.string.inform))
+        alert.setMessage(getApplication<Application>().resources.getString(R.string.really_delete))
+        alert.setButton(AlertDialog.BUTTON_POSITIVE, getApplication<Application>().resources.getString(R.string.ok)) { _, i ->
+            val condition: PultusORMCondition = PultusORMCondition.Builder()
+                    .eq("noteId", note.noteId)
+                    .build()
+            pultusORM.delete(Note(), condition)
+            Toast.makeText(getApplication(), getApplication<Application>().resources.getString(R.string.delete_finish), Toast.LENGTH_SHORT).show()
+            finishCallback.call()
+        }
+        alert.setButton(AlertDialog.BUTTON_NEGATIVE, getApplication<Application>().resources.getString(R.string.cancle)) { _, i ->
+            alert.dismiss()
+        }
+        alert.show()
+
+    }
+}
